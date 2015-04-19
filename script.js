06dbe9ad98d398469e361eb06cb48255040ebe0a
5nahalf <5nahalf@comcast.net>
2015-04-18 21:28:01 -0500

    add func to js

---
+++ script.js
@@ -0,0 +1,14 @@
+function Monster (name, strength, dexterity, constitution, wisdom, intelligence, charisma) {
+	this.name = name;
+	this.strength = strength;
+	this.dexterity = dexterity;
+	this.constitution = constitution;
+	this.wisdom = wisdom;
+	this.intelligence = intelligence;
+	this.charisma = charisma;
+
+
+};
+
+
+function Player (playerClass, )
